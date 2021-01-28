78e1c032506a02b815a7109f4b4305a90b54fce9
YihaoChen96 <yc2288@cornell.edu>
2021-01-28 11:42:14 -0500

    finished

---
+++ assignment01/Q1.py
@@ -0,0 +1,50 @@
+from scipy.signal import correlate, correlation_lags
+from scipy.io.wavfile import read as readwav
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def crossCorr(x, y):
+    corr = correlate(x,y)
+    corr /= np.max(corr)
+    return corr
+
+def loadSoundFile(filename):
+    rate, data = readwav(filename)
+
+    # Get left channel
+    origin = data
+    if data.shape[1] > 1:
+        data = data[:,0]
+        
+    # Normalize
+    if data.dtype != 'float32':
+        if data.dtype == 'uint8':
+            bits = 8
+        elif data.dtype == 'int16':
+            bits = 16
+        elif data.dtype == 'int32':
+            bits = 32
+
+        data = data / float(2**(bits - 1))
+
+    if data.dtype == 'uint8':
+        data = data - 1.
+
+    return rate, data
+
+
+def main():
+    snare_rate, snare_data = loadSoundFile("snare.wav")
+    drum_rate, drum_loop = loadSoundFile("drum_loop.wav")
+    corr = crossCorr(snare_data, drum_loop)
+    lags = correlation_lags(len(drum_loop), len(snare_data))
+
+    # Plot figure
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(lags, corr)
+    ax.set_title("Cross-Correlated Signal")
+    ax.set_xlabel("Lag")
+    plt.savefig("results/01-correlation.png", format = "png")
+    
---
+++ assignment01/Q2.py
@@ -0,0 +1,15 @@
+from Q1 import crossCorr, loadSoundFile
+from scipy.signal import find_peaks
+import matplotlib.pyplot as plt
+
+def findSnarePosition(snareFilename, drumloopFilename):
+    snare_rate, snare_data = loadSoundFile(snareFilename)
+    drum_rate, drum_loop = loadSoundFile(drumloopFilename)
+    corr = crossCorr(snare_data, drum_loop)
+    peaks, properties = find_peaks(corr, height = 0.75)
+    return list(peaks)
+
+if __name__ == "main":
+    peaks = findSnarePosition("snare.wav", "drum_loop.wav")
+    with open("results/02-snareLocation.txt", "w") as f:
+        f.writelines("%s\n" % p for p in peaks)
